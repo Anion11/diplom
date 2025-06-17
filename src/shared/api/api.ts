e581450472b6ed207f4faf36ae56ef3320518a13
nikita.k.dterra <klukoyt@dterra.ru>
2025-06-18 02:28:28 +0300

    upd

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -2,8 +2,5 @@ import axios from 'axios';
 
 // 217.25.225.213 insureflow.ru
 export const $api = axios.create({
-  baseURL: 'https://insureflow.ru',
-  headers: {
-    Host: 'insureflow.ru'
-  }
+  baseURL: import.meta.env.VITE_API_BASE_URL
 });
